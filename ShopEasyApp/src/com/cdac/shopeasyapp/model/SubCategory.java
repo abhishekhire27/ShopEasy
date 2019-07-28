52223545d6a8962c372763569d2c14c2ab771fad
abhishekhire27 <53362511+abhishekhire27@users.noreply.github.com>
2019-07-28 18:05:33 +0530

    Add files via upload

---
+++ ShopEasyApp/src/com/cdac/shopeasyapp/model/SubCategory.java
@@ -0,0 +1,66 @@
+ package com.cdac.shopeasyapp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="sub_category")
+public class SubCategory {
+	
+	@Id
+	@Column(name = "subcategory_id")
+	@GeneratedValue
+	private int subCategoryId;
+	
+	@Column(name = "subcategory_name")
+	private String subCategoryName;
+	
+	@Column(name = "category_id")
+	private int categoryId;
+	
+//	@OneToMany
+//	@JoinColumn(name ="subcategory_id")
+//	private Set<Product> products = new HashSet<Product>();
+
+	public SubCategory() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public SubCategory(int subCategoryId, String subCategoryName, int categoryId) {
+		super();
+		this.subCategoryId = subCategoryId;
+		this.subCategoryName = subCategoryName;
+		this.categoryId = categoryId;
+	}
+
+	public int getSubCategoryId() {
+		return subCategoryId;
+	}
+
+	public void setSubCategoryId(int subCategoryId) {
+		this.subCategoryId = subCategoryId;
+	}
+
+	public String getSubCategoryName() {
+		return subCategoryName;
+	}
+
+	public void setSubCategoryName(String subCategoryName) {
+		this.subCategoryName = subCategoryName;
+	}
+
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	
+	
+}
---
+++ ShopEasyApp/src/com/cdac/shopeasyapp/model/User.java
@@ -0,0 +1,162 @@
+package com.cdac.shopeasyapp.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+
+@Entity
+@Table(name = "user")
+public class User {
+	
+	@Column(name = "user_id")
+	private String userId;
+	
+	@Column(name = "user_password")
+	private String userPassword;
+	
+	@Column(name = "user_firstname")
+	private String userFirstName;
+	
+	@Column(name = "user_lastname")
+	private String userLastName;
+	
+	@Column(name = "user_email")
+	private String userEmail;
+	
+	@Column(name = "user_phone")
+	private String userPhone;
+	
+	@Column(name = "user_address")
+	private String userAddress;
+	
+	@Column(name = "user_status")
+	private String userStatus;
+	
+	@Column(name  = "role")
+	private String role;
+	
+	@Id
+	@Column(name = "cart_id")
+	@GeneratedValue
+	private int cartId;
+	
+	@ManyToMany
+	@JoinTable(name = "cart_products", joinColumns = {@JoinColumn(name="cart_id")}, inverseJoinColumns = {@JoinColumn(name="product_id")})
+	private Set<Product> products = new HashSet<Product>();
+	
+	
+	public User() {
+		super();
+	}
+	
+	public User(String userId) {
+		super();
+		this.userId = userId;
+	}
+
+	public User(String userId, String userPassword, String userFirstName, String userLastName, String userEmail,
+			String userPhone, String userAddress, int cartId, String userStatus, String role) {
+		super();
+		this.userId = userId;
+		this.userPassword = userPassword;
+		this.userFirstName = userFirstName;
+		this.userLastName = userLastName;
+		this.userEmail = userEmail;
+		this.userPhone = userPhone;
+		this.userAddress = userAddress;
+		this.cartId = cartId;
+		this.userStatus = userStatus;
+		this.role = role;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public String getUserFirstName() {
+		return userFirstName;
+	}
+
+	public void setUserFirstName(String userFirstName) {
+		this.userFirstName = userFirstName;
+	}
+
+	public String getUserLastName() {
+		return userLastName;
+	}
+
+	public void setUserLastName(String userLastName) {
+		this.userLastName = userLastName;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserPhone() {
+		return userPhone;
+	}
+
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+
+	public String getUserAddress() {
+		return userAddress;
+	}
+
+	public void setUserAddress(String userAddress) {
+		this.userAddress = userAddress;
+	}
+
+	public int getCartId() {
+		return cartId;
+	}
+
+	public void setCartId(int cartId) {
+		this.cartId = cartId;
+	}
+
+	public String getUserStatus() {
+		return userStatus;
+	}
+
+	public void setUserStatus(String userStatus) {
+		this.userStatus = userStatus;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	
+	
+}
